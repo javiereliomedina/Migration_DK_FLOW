87e407ed1257f323980061feddf565a0060c4605
Javier Elio <javiereliomedina@gmail.com>
2020-11-05 20:09:32 +0100

    Foreign citizens in Denmark

--- Code/Pop_migr_LAUs.R
+++ Code/Pop_migr_LAUs.R
@@ -48,7 +48,7 @@
   id_muni <- as.numeric(var_pop$values[[1]]$id)
   id_muni <- id_muni[id_muni > 100]
   ## Gender: Men/Women
-  id_gender <- c(1, 2)
+  id_gender <- NA
   ## Data by countries (remove total)
   id_citizen <- as.numeric(var_pop$values[[4]]$id)
   id_citizen <- id_citizen[id_citizen > 0]
@@ -62,6 +62,7 @@
   
 # Clean column names and format some data
   pop_ctzn %>% 
+    filter(INDHOLD > 0) %>% 
     rename(LAU_NAME = OMRÅDE,
            Gender = KØN,
            Citizen = STATSB,
@@ -74,7 +75,7 @@
   
 # Foreign population ---- 
   pop_ctzn %>% 
-    filter(Citizen != "Denmark", Pop > 0) %>% 
+    filter(Citizen != "Denmark") %>% 
     # Add pop in percentage
     group_by(LAU_NAME, Date) %>% 
     mutate(Pop_pct = Pop / sum(Pop) * 100) %>% 
@@ -87,65 +88,39 @@
     ungroup() %>% 
     group_by(Date) %>% 
     mutate(Pop_pct = 100 * Pop/sum(Pop)) %>% 
-    # New column with the total % of migration (Women + Men)    
-    group_by(Date, Citizen) %>% 
-    mutate(Pop_pct_total = sum(Pop_pct)) %>%
-    ungroup() %>% 
-    arrange(Citizen, Date, Pop_pct_total) -> pop_migr_DK
+    arrange(Citizen, Date, Pop_pct) -> pop_migr_DK
   
-## Bar charts ranking origin countries ---- 
-## Aux. function (bar charts- Top 20 countries)
-  plot_rank_countries <- function(date) {
-    p <- filter(pop_migr_DK, Date == as.Date(date)) %>% 
-      top_n(Pop_pct_total, n = 40) %>% 
-      ggplot(aes(y = fct_reorder(stringr::str_wrap(Citizen, 15), Pop_pct_total),
-                 x = Pop/1000, fill = Gender)) + 
-      geom_bar(stat = "identity") +
-      scale_x_continuous(limits = c(0, 45)) +
-      labs(title = "Top 20 migration countries by origin", 
-           subtitle = paste("Date", date, sep = ": "),
-           y = "", 
-           x = "Pop [x1000]") +
-      theme_bw() +
-      theme(axis.text = element_text(size = 8),
-            axis.title = element_text(size = 11, face ="bold"))
-    print(p)
-  }
-  
-  # Baseline (2008-Q1)
-  p1 <- plot_rank_countries("2008-01-01")
-  # 2012
-  p2 <- plot_rank_countries("2012-01-01") +
-    labs(title = "")
-  # 2016
-  p3 <- plot_rank_countries("2016-01-01") +
-    labs(title = "")
-  # Actual stage (2020)
-  p4 <- plot_rank_countries("2020-01-01") +
-    labs(title = "")
-   
-  (p1 + p2) / (p3 + p4) + plot_layout(guides = "collect") 
-  ggsave("Results/top_20_migr_countries_2008_2020.png", width = 35, height = 25, units = "cm")
-  
-# Animation
-  dates <- unique(pop_ctzn$Date)
-  gif <- function() {lapply(dates, function(i) {plot_rank_countries(i)})}
-  animation::saveGIF(gif(), interval = .5, movie.name = "top_20_mifr_countries_2008_2020.gif")
-
 ## Plot changes over time (by gender)
 ## Only plot counties with a population >8000 at any time
   pop_migr_DK %>%
+    pivot_wider(c(Citizen, Date), names_from = Gender, values_from = Pop) %>% 
     group_by(Date) %>%
-    filter(Pop > 8000) %>%
+    filter(Total > 14000) -> pop_migr_14k
+  
+  pop_migr_14k %>%
+    ggplot() +
+    geom_line(aes(x = Date, y = Total/1000, colour = Citizen)) +
+    theme_bw() +
+    labs(title = "Foreign citizens in Denmark", 
+         subtitle = "Population higher than 14k",
+         y = "Pop [x1000]") +
+    theme(axis.text = element_text(size = 8),
+          axis.title = element_text(size = 11, face ="bold")) +
+    scale_color_brewer(palette = "Paired") -> p1
+  
+  pop_migr_14k %>%
+    select(-Total) %>% 
+    pivot_longer(cols = -c(Citizen, Date), names_to = "Gender", values_to = "Pop") %>% 
     ggplot() +
     geom_line(aes(x = Date, y = Pop/1000, colour = Citizen)) +
     facet_grid(~Gender) +
     theme_bw() +
-    labs(title = "Foreign citizens by gender in Denmark", 
-         subtitle = "Population higher than 8000 at any time",
+    labs(subtitle = "Divided by gender", 
          y = "Pop [x1000]") +
     theme(axis.text = element_text(size = 8),
           axis.title = element_text(size = 11, face ="bold")) +
-    scale_color_brewer(palette = "Paired")
-  ggsave("Results/pop_migr_countries_2008_2020.png", width = 18, height = 10, units = "cm")
+    scale_color_brewer(palette = "Paired") -> p2
+  
+  p1 / p2 + plot_layout(guides = "collect") 
+  ggsave("Results/pop_migr_countries_2008_2020.png", width = 20, height = 20, units = "cm")
   
