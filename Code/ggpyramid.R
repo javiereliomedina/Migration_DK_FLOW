91912a43a2f93372c4cfddc7a7abd77f6f9716bc
Javier Elio <javiereliomedina@gmail.com>
2020-11-12 14:01:28 +0100

    Remove annotate

--- Code/ggpyramid.R
+++ Code/ggpyramid.R
@@ -7,38 +7,21 @@ ggpyramid <- function(data = data,
                       gender = gender, 
                       men = "Men",
                       women = "Women",
-                      fill = gender,
-                      annotate = FALSE) {
+                      fill = gender) {
   p <- ggplot() + 
-      geom_bar(data = subset( {{ data }}, {{ gender }} == women ),
-               aes(x = {{ age }},
-                   y = {{ pop }},
-                   fill = {{ fill }}), 
-               stat = "identity", 
-               width = 1) + 
-      geom_bar(data = subset( {{ data }}, {{ gender }} == men ),
-               aes(x = {{ age }},
-                   y = - {{ pop }},
-                   fill = {{ fill }}), 
-               stat = "identity",
-               width = 1) + 
-      geom_hline(yintercept = 0, colour = "grey10") +
-      scale_x_discrete(name = "Age") +
-      coord_flip() 
-
-  if(annotate) {
-    p +
-      annotate(geom = "text",
-               y = max(data$pop) / 2,
-               x = max(as.numeric(data$age)),
-               label = women,
-               fontface = "bold") +
-      annotate(geom = "text",
-               y = -max(data$pop)/ 2 ,
-               x = max(as.numeric(data$age)),
-               label = men,
-               fontface = "bold") 
-   } else{
-     p
-   }
+    geom_bar(data = subset( {{ data }}, {{ gender }} == women ),
+             aes(x = {{ age }},
+                 y = {{ pop }},
+                 fill = {{ fill }}), 
+             stat = "identity", 
+             width = 1) + 
+    geom_bar(data = subset( {{ data }}, {{ gender }} == men ),
+             aes(x = {{ age }},
+                 y = - {{ pop }},
+                 fill = {{ fill }}), 
+             stat = "identity",
+             width = 1) + 
+    geom_hline(yintercept = 0, colour = "grey10") +
+    scale_x_discrete(name = "Age") +
+    coord_flip() 
 }
